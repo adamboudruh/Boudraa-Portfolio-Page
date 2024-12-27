a67b1d5ef02a520b1ffde0057ada9f212a7d95db
Adam Boudraa <adamboudraa@gmail.com>
2024-12-27 12:53:52 -0800

    Update projects and resume

--- assets/projectList.js
+++ assets/projectList.js
@@ -3,51 +3,54 @@ import social from '../assets/images/project-ss/social-network.png';
 import win from '../assets/images/project-ss/winning-edge.png';
 import wife from '../assets/images/project-ss/kitten.jpg';
 import weather from '../assets/images/project-ss/weather.png';
-import employee from '../assets/images/project-ss/employee-tracker.jpg';
+import fit from '../assets/images/project-ss/fitconnectss.png';
+
+// import employee from '../assets/images/project-ss/employee-tracker.jpg';
 
 const projects = [
+    
     {
         id: 1,
+        title: "Fit Connect",
+        githubLink: "https://github.com/cbursch13/Fit-Connect",
+        deployedLink: "https://fit-connect.onrender.com/",
+        img: fit
+    },
+    {
+        id: 2,
         title: "Blog Page",
         githubLink: "https://github.com/adamboudruh/Boudraa-Blog-Page",
         deployedLink: "https://evening-tundra-87710-7eb66c5c7750.herokuapp.com/",
         img: blog
     },
     {
-        id: 2,
+        id: 3,
         title: "Social Network API",
         githubLink: "https://github.com/adamboudruh/Boudraa-Social-Network-API",
         deployedLink: "https://github.com/adamboudruh/Boudraa-Social-Network-API",
         img: social
     },
     {
-        id: 3,
+        id: 4,
         title: "Winning Edge",
         githubLink: "https://github.com/adamboudruh/Group-1-Winning-Edge",
         deployedLink: "https://pacific-shelf-77218-ba08c8175600.herokuapp.com/",
         img: win
     },
     {
-        id: 4,
+        id: 5,
         title: "Website for my wife",
         githubLink: "https://github.com/adamboudruh/Mallorys_Website",
         deployedLink: "https://adamboudruh.github.io/Mallorys_Website/index",
         img: wife
     },
     {
-        id: 5,
+        id: 6,
         title: "Weather Dashboard",
         githubLink: "https://github.com/adamboudruh/Boudraa-Weather-Dashboard",
         deployedLink: "https://adamboudruh.github.io/Boudraa-Weather-Dashboard/",
         img: weather
-    },
-    {
-        id: 6,
-        title: "Employee Tracker",
-        githubLink: "https://github.com/adamboudruh/Boudraa-Employee-Tracker",
-        deployedLink: "https://github.com/adamboudruh/Boudraa-Employee-Tracker",
-        img: employee
-    },
+    }
 ]
 
 export default projects;
