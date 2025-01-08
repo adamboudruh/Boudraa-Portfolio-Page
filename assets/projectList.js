5b0a8fc70fb277b9d338f1f08a3590cffff7eb64
Adam Boudraa <adamboudraa@gmail.com>
2025-01-08 00:29:45 -0800

    Add myPlanner to project list

--- assets/projectList.js
+++ assets/projectList.js
@@ -1,9 +1,10 @@
 import blog from '../assets/images/project-ss/blog.png';
 import social from '../assets/images/project-ss/social-network.png';
 import win from '../assets/images/project-ss/winning-edge.png';
-import wife from '../assets/images/project-ss/kitten.jpg';
-import weather from '../assets/images/project-ss/weather.png';
+// import wife from '../assets/images/project-ss/kitten.jpg';
+// import weather from '../assets/images/project-ss/weather.png';
 import fit from '../assets/images/project-ss/fitconnectss.png';
+import plan from '../assets/images/project-ss/myPlannerSS.png';
 
 // import employee from '../assets/images/project-ss/employee-tracker.jpg';
 
@@ -11,46 +12,53 @@ const projects = [
     
     {
         id: 1,
+        title: "myPlanner",
+        githubLink: "https://github.com/adamboudruh/Boudraa-myPlanner",
+        deployedLink: "https://github.com/adamboudruh/Boudraa-myPlanner",
+        img: plan
+    },
+    {
+        id: 2,
         title: "Fit Connect",
         githubLink: "https://github.com/cbursch13/Fit-Connect",
         deployedLink: "https://fit-connect.onrender.com/",
         img: fit
     },
     {
-        id: 2,
+        id: 3,
         title: "Blog Page",
         githubLink: "https://github.com/adamboudruh/Boudraa-Blog-Page",
         deployedLink: "https://evening-tundra-87710-7eb66c5c7750.herokuapp.com/",
         img: blog
     },
     {
-        id: 3,
-        title: "Social Network API",
+        id: 4,
+        title: "Social Network REST API",
         githubLink: "https://github.com/adamboudruh/Boudraa-Social-Network-API",
         deployedLink: "https://github.com/adamboudruh/Boudraa-Social-Network-API",
         img: social
     },
     {
-        id: 4,
+        id: 5,
         title: "Winning Edge",
         githubLink: "https://github.com/adamboudruh/Group-1-Winning-Edge",
         deployedLink: "https://pacific-shelf-77218-ba08c8175600.herokuapp.com/",
         img: win
     },
-    {
-        id: 5,
-        title: "Website for my wife",
-        githubLink: "https://github.com/adamboudruh/Mallorys_Website",
-        deployedLink: "https://adamboudruh.github.io/Mallorys_Website/index",
-        img: wife
-    },
-    {
-        id: 6,
-        title: "Weather Dashboard",
-        githubLink: "https://github.com/adamboudruh/Boudraa-Weather-Dashboard",
-        deployedLink: "https://adamboudruh.github.io/Boudraa-Weather-Dashboard/",
-        img: weather
-    }
+    // {
+    //     id: 5,
+    //     title: "Website for my wife",
+    //     githubLink: "https://github.com/adamboudruh/Mallorys_Website",
+    //     deployedLink: "https://adamboudruh.github.io/Mallorys_Website/index",
+    //     img: wife
+    // },
+    // {
+    //     id: 6,
+    //     title: "Weather Dashboard",
+    //     githubLink: "https://github.com/adamboudruh/Boudraa-Weather-Dashboard",
+    //     deployedLink: "https://adamboudruh.github.io/Boudraa-Weather-Dashboard/",
+    //     img: weather
+    // }
 ]
 
 export default projects;
